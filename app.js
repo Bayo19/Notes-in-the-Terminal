ebe273d165887c7b26277a445f1a8ce068fb8644
Bayo Ade <bayo.300@gmail.com>
2020-02-08 12:31:39 +0000

    added more colors and functionality like clearing whole list and making node app executable

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
-// const validator = require('validator')
+#!/usr/bin/env node
+
 const yargs = require('yargs')
 const chalk = require('chalk')
 const notes = require('./notes')
 console.clear()
 
-// const good = chalk.underline.magenta;
-// const bad = chalk.underline.red;
-// const other = chalk.bgYellow.black;
-// console.log(good('Broski'))
-// console.log(other('Brahstein'))
-// console.log(bad('Errorstein'))
 
-// customize yargs version
-yargs.version('1.1.0')
-    // setting up yargs to work with commands, add, read, list, delete
-    // create add command
+
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
@@ -29,6 +21,9 @@ yargs.command({
             demandOption: true,
             type: 'string'
 
+        },
+        time: {
+            describe: 'The time of note added'
         }
     },
     handler(argv) {
@@ -74,10 +69,14 @@ yargs.command({
     }
 })
 
+yargs.command({
+    command: 'clearlist',
+    describe: 'clears the lists so that it\'s empty',
+    handler(argv) {
+        notes.clearNotes()
+    }
+})
+
 yargs.parse()
 
 //-----------------
-
-const square = (x) => {
-    return x * x
-}
--- notes.js
+++ notes.js
@@ -5,25 +5,31 @@ let getNotes = (note) => {
     return 'Your notes ...'
 }
 
-const addNote = (title, body) => {
+const bodycapitalized = (s) => {
+    if (typeof s !== 'string') {
+        return ''
+    } else {
+        return s.charAt(0).toUpperCase() + s.slice(1)
+    }
+}
+
+const addNote = (title, body, time) => {
     const notes = loadNotes()
-        // const duplicateNotes = notes.filter((note) => {
-        //     return note.title === title
-        // })
+
     const duplicateNote = notes.find((note) => {
             return note.title === title
         })
         // find() returns the first value of first element that satisfies provided testing function
-
     if (!duplicateNote) {
 
         notes.push({
             title: title,
-            body: body
+            body: body,
+            time: Date()
         })
 
         saveNotes(notes)
-        console.log(chalk.green('new note added'))
+        console.log(chalk.rgb(34, 171, 220)('new note added'))
 
     } else {
         console.log(chalk.red('note title already exists'))
@@ -39,7 +45,7 @@ const removeNote = (title) => {
 
     if (notes.length > notes2.length) {
         saveNotes(notes2)
-        console.log(chalk.green(`${title} has been removed...`))
+        console.log(chalk.rgb(58, 162, 84)(`${title} has been removed...`))
     } else {
         console.log(chalk.red('no note found'))
 
@@ -68,13 +74,20 @@ const loadNotes = () => {
 
 }
 
+const clearNotes = () => {
+    fs.writeFileSync('notes.json', [])
+    console.log(chalk.rgb(255, 255, 240)('The list has been cleared'))
+}
+
 const listNotes = () => {
     const notes = loadNotes()
-    console.log(chalk.yellow('Your Notes'))
+    console.log(chalk.rgb(255, 255, 240)('Your Notes'))
 
     notes.forEach((note) => {
-        console.log(chalk.blue(note.title.toUpperCase()))
-        console.log(chalk.magenta(note.body))
+        console.log(chalk.rgb(124, 216, 65)(note.title.toUpperCase()))
+        console.log(chalk.rgb(239, 64, 175)(bodycapitalized(note.body)))
+
+        console.log(chalk.rgb(250, 218, 94)(note.time))
     })
 
 
@@ -86,13 +99,6 @@ const readNote = (title) => {
         return Anote.title === title
     })
 
-    const bodycapitalized = (s) => {
-        if (typeof s !== 'string') {
-            return ''
-        } else {
-            return s.charAt(0).toUpperCase() + s.slice(1)
-        }
-    }
 
     if (notetoRead) {
         console.log(chalk.yellow(notetoRead.title.toUpperCase()))
@@ -108,6 +114,7 @@ module.exports = {
     addNote: addNote,
     removeNote: removeNote,
     listNotes: listNotes,
-    readNote: readNote
+    readNote: readNote,
+    clearNotes: clearNotes
 
 }
